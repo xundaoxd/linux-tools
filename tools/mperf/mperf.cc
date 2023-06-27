4af7bb84b598fd66ad8ae1023375ed4cf5ce2276
yinchaogao <yinchaogao@deeproute.ai>
2023-06-27 10:43:25 +0800

    strip code

--- tools/mperf/mperf.cc
+++ tools/mperf/mperf.cc
@@ -1,11 +1,8 @@
 #include <iostream>
 
-#include <gflags/gflags.h>
-
 #include "common.h"
 
 int main(int argc, char *argv[]) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
   std::cout << "mperf" << std::endl;
   return 0;
 }
