97e9a5001a80cdd5691a1fc5a1893a6698807f9b
xundaoxd <xundaoxd@outlook.com>
2023-08-14 15:24:08 +0800

    setup ebpf

---
+++ ebpf/minimal/minimal.bpf.c
@@ -0,0 +1,18 @@
+#include <bpf/bpf_helpers.h>
+#include <linux/bpf.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+int my_pid = 0;
+
+SEC("tp/syscalls/sys_enter_write")
+int handle_tp(void *ctx) {
+  int pid = bpf_get_current_pid_tgid() >> 32;
+
+  if (pid != my_pid)
+    return 0;
+
+  bpf_printk("BPF triggered from PID %d.\n", pid);
+
+  return 0;
+}
